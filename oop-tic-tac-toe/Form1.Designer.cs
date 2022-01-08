9602a3f7a64db029a494ff3e3ee2d1a84708429c
benedictfernando <fernandobenedict619@gmail.com>
2022-01-09 00:29:04 +0800

    layout & design tic-tac-toe cells

--- oop-tic-tac-toe/Form1.Designer.cs
+++ oop-tic-tac-toe/Form1.Designer.cs
@@ -29,24 +29,185 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(tictactoe));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tl = new System.Windows.Forms.Button();
+            this.t = new System.Windows.Forms.Button();
+            this.tr = new System.Windows.Forms.Button();
+            this.l = new System.Windows.Forms.Button();
+            this.m = new System.Windows.Forms.Button();
+            this.r = new System.Windows.Forms.Button();
+            this.bl = new System.Windows.Forms.Button();
+            this.b = new System.Windows.Forms.Button();
+            this.br = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.Controls.Add(this.tl, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.t, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tr, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.l, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.m, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.r, 3, 2);
+            this.tableLayoutPanel1.Controls.Add(this.bl, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.b, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.br, 3, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(480, 457);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tl
+            // 
+            this.tl.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tl.Font = new System.Drawing.Font("Century Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tl.Location = new System.Drawing.Point(63, 60);
+            this.tl.Name = "tl";
+            this.tl.Size = new System.Drawing.Size(114, 108);
+            this.tl.TabIndex = 0;
+            this.tl.Text = "O";
+            this.tl.UseVisualStyleBackColor = true;
+            // 
+            // t
+            // 
+            this.t.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.t.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.t.Font = new System.Drawing.Font("Century Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.t.Location = new System.Drawing.Point(183, 60);
+            this.t.Name = "t";
+            this.t.Size = new System.Drawing.Size(114, 108);
+            this.t.TabIndex = 1;
+            this.t.Text = "X";
+            this.t.UseVisualStyleBackColor = true;
+            // 
+            // tr
+            // 
+            this.tr.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tr.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tr.Font = new System.Drawing.Font("Century Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tr.Location = new System.Drawing.Point(303, 60);
+            this.tr.Name = "tr";
+            this.tr.Size = new System.Drawing.Size(114, 108);
+            this.tr.TabIndex = 2;
+            this.tr.Text = "O";
+            this.tr.UseVisualStyleBackColor = true;
+            // 
+            // l
+            // 
+            this.l.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.l.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.l.Font = new System.Drawing.Font("Century Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.l.Location = new System.Drawing.Point(63, 174);
+            this.l.Name = "l";
+            this.l.Size = new System.Drawing.Size(114, 108);
+            this.l.TabIndex = 3;
+            this.l.Text = "O";
+            this.l.UseVisualStyleBackColor = true;
+            // 
+            // m
+            // 
+            this.m.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.m.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.m.Font = new System.Drawing.Font("Century Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.m.Location = new System.Drawing.Point(183, 174);
+            this.m.Name = "m";
+            this.m.Size = new System.Drawing.Size(114, 108);
+            this.m.TabIndex = 4;
+            this.m.Text = "X";
+            this.m.UseVisualStyleBackColor = true;
+            // 
+            // r
+            // 
+            this.r.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.r.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.r.Font = new System.Drawing.Font("Century Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.r.Location = new System.Drawing.Point(303, 174);
+            this.r.Name = "r";
+            this.r.Size = new System.Drawing.Size(114, 108);
+            this.r.TabIndex = 5;
+            this.r.Text = "O";
+            this.r.UseVisualStyleBackColor = true;
+            // 
+            // bl
+            // 
+            this.bl.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bl.Font = new System.Drawing.Font("Century Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.bl.Location = new System.Drawing.Point(63, 288);
+            this.bl.Name = "bl";
+            this.bl.Size = new System.Drawing.Size(114, 108);
+            this.bl.TabIndex = 6;
+            this.bl.Text = "X";
+            this.bl.UseVisualStyleBackColor = true;
+            // 
+            // b
+            // 
+            this.b.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.b.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.b.Font = new System.Drawing.Font("Century Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.b.Location = new System.Drawing.Point(183, 288);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(114, 108);
+            this.b.TabIndex = 7;
+            this.b.Text = "O";
+            this.b.UseVisualStyleBackColor = true;
+            // 
+            // br
+            // 
+            this.br.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.br.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.br.Font = new System.Drawing.Font("Century Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.br.Location = new System.Drawing.Point(303, 288);
+            this.br.Name = "br";
+            this.br.Size = new System.Drawing.Size(114, 108);
+            this.br.TabIndex = 8;
+            this.br.Text = "X";
+            this.br.UseVisualStyleBackColor = true;
             // 
             // tictactoe
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(480, 457);
-            this.Font = new System.Drawing.Font("Cascadia Code", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "tictactoe";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tic-Tac-Toe";
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button tl;
+        private System.Windows.Forms.Button t;
+        private System.Windows.Forms.Button tr;
+        private System.Windows.Forms.Button l;
+        private System.Windows.Forms.Button m;
+        private System.Windows.Forms.Button r;
+        private System.Windows.Forms.Button bl;
+        private System.Windows.Forms.Button b;
+        private System.Windows.Forms.Button br;
     }
 }

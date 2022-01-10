1e54bc54705345ef0d23c2d710c336ac74f457d6
benedictfernando <fernandobenedict619@gmail.com>
2022-01-10 20:02:17 +0800

    use prototype for checking winner status

--- oop-tic-tac-toe/Form1.cs
+++ oop-tic-tac-toe/Form1.cs
@@ -12,12 +12,27 @@ namespace oop_tic_tac_toe
 {
     public partial class tictactoe : Form
     {
+        private bool p1Turn = true;
+        private List<Button> cells = new List<Button>();
+
         public tictactoe()
         {
             InitializeComponent();
-        }
+            
+            // Add buttons to 'cells' global variable
+            cells.Add(this.tl);
+            cells.Add(this.t);
+            cells.Add(this.tr);
+            cells.Add(this.l);
+            cells.Add(this.m);
+            cells.Add(this.r);
+            cells.Add(this.bl);
+            cells.Add(this.b);
+            cells.Add(this.br);
 
-        private bool p1Turn = true;
+            // Start game
+            // newGame.PerformClick()
+        }
 
         private void cell_Click(object sender, EventArgs e)
         {
@@ -29,7 +44,56 @@ namespace oop_tic_tac_toe
 
                 button.Enabled = false;
                 p1Turn = !p1Turn;
-                // checkForWinner();
+                checkForWinner();
+            }
+        }
+
+        private void checkForWinner()
+        {
+            bool tie = true;
+            string result = null;
+
+            // Check for any enabled cells
+            foreach (var cell in cells)
+            {
+                if (cell.Enabled) { tie = false; break; }
+            }
+
+            if (tie) result = "Tie!";
+            else    // if result is not 'tie'...
+            {
+                string l = cells[3].Text, m = cells[4].Text, r = cells[5].Text,
+                tl = cells[0].Text, t = cells[1].Text, tr = cells[2].Text,
+                bl = cells[6].Text, b = cells[7].Text, br = cells[8].Text;
+
+                // for trios with 'middle' cell as their common one
+                if ((tl == m && m == br || t == m && m == b || tr == m && m == bl 
+                    || r == m && m == l) && !cells[4].Enabled) result = m;
+
+                // for trios with 'top-left' cell as their common one
+                else if ((tl == t && t == tr || tl == l && l == bl)
+                    && !cells[0].Enabled) result = tl;
+
+                // for trios with 'bottom-right' cell as their common one
+                else if ((br == r && r == tr || br == b && b == bl)
+                    && !cells[8].Enabled) result = br;
+            }
+
+            if (result != null)
+            {
+                if (result == "X")
+                {
+                    result = "Player 1 wins!"; // p1Score++;
+                }
+                else if (result == "O")
+                {
+                    result = "Player 2 wins!"; // p2Score++;
+                }
+
+                if (MessageBox.Show(result) == DialogResult.OK)
+                {
+                    // newGame.PerformClick()
+                }
             }
         }
 
